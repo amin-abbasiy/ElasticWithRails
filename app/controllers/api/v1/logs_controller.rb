4ff24fe017d0392acf17e4a9fdc5488577e9cb2e
amin-abbasiy <uamin.en@gmail.com>
2023-01-22 15:36:18 +0000

    save request and add tranlation files

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::LogsController < Api::V1::BaseController
   def create
-    @log = ::Logs.new(whitelist_params, self).call
+    @log = ::Logs::Create.new(whitelist_params, self).call
 
     render_formatted_output @log.response.body, @log.status
   end
 
   def search
-    @logs = ::LogSearch.new(params[:term], self).call
+    @logs = ::Logs::LogSearch.new(params[:term], self).call
 
     render_formatted_output @logs.response.body, @log.status
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,28 @@
 class ApplicationController < ActionController::Base
-  rescue_from :JSONError do |e|
+  before_action :capture_data
+
+  rescue_from ::JSON::JSONError do |e|
     render_error e.message, :bad_request
   end
   def home
-    render json: { message: "Hello and Welcome to Elastic with Rails" }
+    render json: { message: I18n.t("messages.welcome") }
   end
 
   protected
+  def save_data
+    data = {
+            ip: request.remote_ip,
+            path: request.path,
+            creation_time: Time.now,
+            request_info: {
+              request_id: request.request_id,
+              method: request.method,
+              url: request.url
+            }
+          }
+
+    ::Logs::Create.new(data, self).call
+  end
 
   def render_formatted_output(data, status)
     render json: { logs: data }, status: status
@@ -15,6 +31,7 @@ class ApplicationController < ActionController::Base
   private
 
   def render_error(massage, code)
-    return render json: { error: I18n.t("errors.#{code.to_s}")}, status: code
+    return render json: { error: { code: I18n.t("errors.#{code.to_s}") }
+                                   .merge(message: massage) }, status: code
   end
 end
